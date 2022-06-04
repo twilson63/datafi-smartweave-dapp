3b355f9009d31a011bd40bba93dddf28e39e0644
Tom Wilson <tom@hyper.io>
2022-06-04 20:38:37 +0000

    updated deploy script to try to use redstone gateway

--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 import Arweave from 'arweave'
 import fs from 'fs'
 import Bundlr from '@bundlr-network/client';
+import fetch from 'node-fetch'
 
 const arweave = Arweave.init({
   host: 'arweave.net',
@@ -27,10 +28,14 @@ console.log('contract source', tx.id)
 
 // deploy contract with path.manifest 
 const CONTRACT_SRC = 'N2oVt_uOMhnrCzTMWSZ0lMmgtqegEKoJ_pi7yDOJCkQ'
-const js = 'fc7a161f'
+const js = '666268d5'
+/*
 const bundlr = new Bundlr.default("http://node2.bundlr.network", "arweave", w);
 
+
 const indexHTML = fs.readFileSync('./dist/index.html', 'utf-8')
+const arweaveJS = fs.readFileSync('./dist/assets/arweave.9daa5074.js', 'utf-8')
+const arWalletJS = fs.readFileSync('./dist/assets/arweave-wallet-connector.4da4094f.js', 'utf-8')
 const indexJS = fs.readFileSync(`./dist/assets/index.${js}.js`, 'utf-8')
 
 // upload index.html
@@ -51,6 +56,24 @@ const indexJSTx = await bundlr.createTransaction(indexJS, {
 await indexJSTx.sign()
 await indexJSTx.upload()
 
+// upload index.js
+const arweaveJSTx = await bundlr.createTransaction(arweaveJS, {
+  tags: [
+    { name: 'Content-Type', value: 'application/javascript' }
+  ]
+})
+await arweaveJSTx.sign()
+await arweaveJSTx.upload()
+
+// upload index.js
+const arWalletJSTx = await bundlr.createTransaction(arWalletJS, {
+  tags: [
+    { name: 'Content-Type', value: 'application/javascript' }
+  ]
+})
+await arWalletJSTx.sign()
+await arWalletJSTx.upload()
+
 const app = JSON.stringify({
   manifest: "arweave/paths",
   version: "0.1.0",
@@ -63,9 +86,17 @@ const app = JSON.stringify({
     },
     [`assets/index.${js}.js`]: {
       "id": `${indexJSTx.id}`
+    },
+    [`assets/arweave.9daa5074.js`]: {
+      "id": `${arweaveJSTx.id}`
+    },
+    [`assets/arweave-wallet-connector.4da4094f.js`]: {
+      "id": `${arWalletJSTx.id}`
     }
   }
 })
+*/
+const app = '{"manifest":"arweave/paths","version":"0.1.0","index":{"path":"index.html"},"paths":{"index.html":{"id":"AaVpAH1gVzCSCj0mPGwqtXY3bQHE97b8LVx4SFpV1TU"},"assets/index.666268d5.js":{"id":"P9szTE_r7gVqiTEzFPblCqwafj8iDsGLeXZ6irJXcww"},"assets/arweave.9daa5074.js":{"id":"JPzs9tGfERZdwlwnMSz4q6bShJGJfzFzLCdDvsQaDqE"},"assets/arweave-wallet-connector.4da4094f.js":{"id":"kZHTLtadgqxnkfTMpK5LGEsfewHHhLEbkP63U83rw2c"}}}'
 
 const appContract = await arweave.createTransaction({ data: app })
 appContract.addTag('Content-Type', 'application/x.arweave-manifest+json')
@@ -76,12 +107,13 @@ appContract.addTag('Init-State', JSON.stringify({ count: 0 }))
 
 
 await arweave.transactions.sign(appContract, w)
-await arweave.transactions.post(appContract)
-
+//await arweave.transactions.post(appContract)
+const result = await fetch('https://gateway.redstone.finance/gateway/contracts/deploy', {
+  method: 'POST',
+  body: JSON.stringify({ contractTx: appContract }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.ok ? res.json() : res)
+console.log(result)
 console.log(appContract.id)
-
-
-
-
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import fs from 'fs'
+
+const { dependencies } = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if ([].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://')
 const publicUrl = `3000-${host}`
@@ -8,6 +19,17 @@ const publicUrl = `3000-${host}`
 export default defineConfig({
   base: '',
   plugins: [svelte()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
   server: {
     hmr: {
       clientPort: host ? 443 : 24678,
